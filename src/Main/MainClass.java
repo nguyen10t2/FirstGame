729a2dfedefa66bd8e4e592f4a715ffecf637bf9
nguien <nguyen10t2lhp@gmail.com>
2024-12-20 19:46:20 +0700

    Oke

--- src/Main/MainClass.java
+++ src/Main/MainClass.java
@@ -3,6 +3,8 @@ package Main;
 import GameWindow.GamePlane;
 
 public class MainClass{
+
+    // Bấm giữ nút EXIT được xem hiệu ứng
     public static void main(String[] args) {
         GamePlane.main(args);
     }
